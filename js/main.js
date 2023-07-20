01b85b73e3cc34fa3ebbba9a6ef4386b1c77f93f
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-07-21 00:28:14 +0200

    add : button create ticket and button reset information

--- js/main.js
+++ js/main.js
@@ -1,51 +1,102 @@
-// Request Km trip
-const tripKm = parseFloat(prompt("Quanti Km devi viaggiare"));
-console.log("Viaggio di " + tripKm + " Km");
-console.log(typeof tripKm);
+// Button send information
+const sendButton = document.getElementById("btn-send");
 
-// Request age
-const ageUtent = prompt("Scrivi la tue età");
-console.log("Età: " + ageUtent);
+// Button reset information
+const resetButton = document.getElementById("btn-reset");
 
-// Price € x km
-const priceKm = 0.21;
-console.log("Prezzo per Km " + priceKm);
-console.log(typeof priceKm);
+// Request informatio
+const nameInput = document.getElementById("text-name");
+const kmInput = document.getElementById("text-km");
+const ageInput = document.getElementById("text-age");
 
-// Ticket standard
-let ticketStandard = priceKm * tripKm;
-console.log("Biglietto standard " + ticketStandard);
-console.log(typeof ticketStandard);
+// Value Information
+let nameUtent;
+let tripKm;
+let ageUtent;
 
-// Discount Age
-let discountApply;
+sendButton.addEventListener("click", function () {
+  // Request Name
+  nameUtent = nameInput.value;
+  console.log("Passeggero:  " + nameUtent);
+  console.log(typeof nameUtent);
 
-if (ageUtent >= 65) {
-  discountApply = ticketStandard * 0.4;
-} else if (ageUtent < 18) {
-  discountApply = ticketStandard * 0.2;
-} else {
-  discountApply = 0;
-}
+  // Request Km trip
+  tripKm = parseFloat(kmInput.value);
+  console.log("Viaggio di " + tripKm + " Km");
+  console.log(typeof tripKm);
 
-console.log("Sconto applicato " + discountApply);
-console.log(typeof discountApply);
+  // Request age
+  ageUtent = parseInt(ageInput.value);
+  console.log("Età: " + ageUtent);
 
-// Ticket Final
+  // Price € x km
+  const priceKm = 0.21;
+  console.log("Prezzo per Km " + priceKm);
+  console.log(typeof priceKm);
 
-let ticketFinal = ticketStandard - discountApply;
+  // Ticket standard
+  let ticketStandard = priceKm * tripKm;
+  console.log("Biglietto standard " + ticketStandard);
+  console.log(typeof ticketStandard);
 
-ticketFinal;
-console.log("Biglietto Finale " + ticketFinal);
-console.log(typeof ticketFinal);
+  // Discount Age
+  let discountApply;
 
-// Ticket client
-const ticketClient = `
+  if (ageUtent >= 65) {
+    discountApply = ticketStandard * 0.4;
+  } else if (ageUtent < 18) {
+    discountApply = ticketStandard * 0.2;
+  } else {
+    discountApply = 0;
+  }
+
+  console.log("Sconto applicato " + discountApply);
+  console.log(typeof discountApply);
+
+  // Ticket Final
+
+  let ticketFinal = ticketStandard - discountApply;
+
+  ticketFinal;
+  console.log("Biglietto Finale " + ticketFinal);
+  console.log(typeof ticketFinal);
+
+  // Ticket client
+  const ticketClient = `
+  <h5> Prezzo biglietto</h5>
+  <p>Nome Passeggero: ${nameUtent}</p>
+
 <p>Biglietto normale: € ${ticketStandard.toFixed(2)}</p>
 <p>Sconto: € ${discountApply.toFixed(2)}</p>
 
 <p>Prezzo biglietto finale: <b> € ${ticketFinal.toFixed(2)}</b></p>
 `;
 
-// PRINT TICKET CLIENT IN HTML WITH ID
-document.getElementById("ticketTrain").innerHTML = ticketClient;
+  // PRINT TICKET CLIENT IN HTML WITH ID
+  if (ticketFinal) {
+    document.getElementById("ticketTrain").innerHTML = ticketClient;
+  } else {
+    document.getElementById("ticketTrain").innerHTML =
+      "Inserisci informazioni necessarie";
+  }
+});
+
+// RESET BUTTON
+
+resetButton.addEventListener("click", function () {
+  // Reset Name
+  nameInput.value = "";
+  console.log("Passeggero:  " + nameInput);
+  console.log(typeof nameInput);
+
+  // Reset Km trip
+  kmInput.value = "";
+  console.log("Viaggio di " + kmInput + " Km");
+  console.log(typeof kmInput);
+
+  // Reset age
+  ageInput.value = "";
+  console.log("Età: " + ageInput);
+
+  document.getElementById("ticketTrain").innerHTML = "";
+});
